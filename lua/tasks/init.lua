e2d8f81b0cb75f59cc547a5cbeaac3621416cc12
Eric Wong <wsdjeg@outlook.com>
2025-03-01 20:47:10 +0800

    typo: runner module name

--- lua/tasks/init.lua
+++ lua/tasks/init.lua
@@ -151,7 +151,7 @@ local function open_task()
   if string.find(line, '^%[.*%]') then
     task = string.sub(vim.fn.matchstr(line, '^\\[.*\\]'), 2, -2)
     vim.cmd('close')
-    require('runner').run_task(expand_task(task_config[task]))
+    require('code-runner').run_task(expand_task(task_config[task]))
   end
 end
 
