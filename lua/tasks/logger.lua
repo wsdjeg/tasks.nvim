569d64295d3aedc90d85233b843cd0406a51df79
Eric Wong <wsdjeg@outlook.com>
2025-04-17 10:23:46 +0800

    fix: implement logger for tasks.nvim

--- lua/tasks/logger.lua
+++ lua/tasks/logger.lua
@@ -1,8 +1,48 @@
 local M = {}
-
-M.debug = function(msg) end
-M.info = function(msg) end
-M.warn = function(msg) end
-M.error = function(msg) end
+local log
+function M.info(msg)
+    if not log then
+        local ok, l = pcall(require, 'logger')
+        if ok then
+            log = l.derive('tasks')
+            log.info(msg)
+        end
+    else
+        log.info(msg)
+    end
+end
+function M.debug(msg)
+    if not log then
+        local ok, l = pcall(require, 'logger')
+        if ok then
+            log = l.derive('tasks')
+            log.debug(msg)
+        end
+    else
+        log.debug(msg)
+    end
+end
+function M.warn(msg)
+    if not log then
+        local ok, l = pcall(require, 'logger')
+        if ok then
+            log = l.derive('tasks')
+            log.warn(msg)
+        end
+    else
+        log.warn(msg)
+    end
+end
+function M.error(msg)
+    if not log then
+        local ok, l = pcall(require, 'logger')
+        if ok then
+            log = l.derive('tasks')
+            log.error(msg)
+        end
+    else
+        log.error(msg)
+    end
+end
 
 return M
